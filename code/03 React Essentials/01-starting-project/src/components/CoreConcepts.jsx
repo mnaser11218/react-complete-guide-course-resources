c071469182e34e1773166af02a15eda2d4a1e632
Mohammed N <mnaser11218@gmail.com>
2025-03-15 00:07:13 -0400

    Added coreconcepts component

---
+++ code/03 React Essentials/01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CoreConcept from './CoreConcept'
+import { CORE_CONCEPTS } from '../data'
+
+function CoreConcepts() {
+  return (
+    <section id="core-concepts">
+    <ul>
+      {CORE_CONCEPTS.map((concept)=> <CoreConcept key={concept.title} {...concept}/>)
+    }
+  
+    </ul>
+    </section>
+  )
+}
+
+export default CoreConcepts
