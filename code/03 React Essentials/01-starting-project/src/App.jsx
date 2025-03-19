b4b88bce57b5b05ff031fdae5fe6710f271b6b65
Mohammed N <mnaser11218@gmail.com>
2025-03-19 01:03:07 -0400

    cleaned up imports

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -1,10 +1,5 @@
-import { Title } from '@mui/icons-material'
-import Img from './assets/react-core-concepts.png'
 import CoreConcepts from './components/CoreConcepts.jsx';
 import Header from './components/Header/Header';
-import TabButton from './components/TabButton';
-import { useState } from 'react';
-import {CORE_CONCEPTS} from './data.js'
 import Examples from './components/Examples.jsx';
 function App() {
   return (
@@ -15,7 +10,7 @@ function App() {
         <Examples/>
       </main>
     </div>
-  );
+  )
 }
 
 export default App;
