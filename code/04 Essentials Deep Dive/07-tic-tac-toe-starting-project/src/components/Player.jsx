5399550b96a62a8a5a9ed306448c2bb6b2f4445f
Mohammed N <mnaser11218@gmail.com>
2025-03-31 22:27:46 -0400

    updated input in player component to update player state when user inputs updated player name

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react"
+import { useState, useEffect} from "react"
 
 export default function Player({name, symbol}){
-    // const [player, setPlayer] = useState(name)
+    const [player, setPlayer] = useState(name)
     const [isEditing, setIsEditing] = useState(false)
-    const inputField = <input></input>
+    const inputField = <input onChange={(e)=>setPlayer(e.target.value)}></input>
+
+   
     return (
         <li>
         <span className="player">
-        <span className="player-name">{ isEditing ? inputField : name }</span>
+        <span className="player-name">{ isEditing ? inputField : player }</span>
         <span className="player-symbol">{symbol}</span>
         </span>
         <button onClick={()=> setIsEditing(!isEditing)}>Edit</button>
