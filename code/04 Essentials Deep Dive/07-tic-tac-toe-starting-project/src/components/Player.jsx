184655fb1084ad94cf442921dfb321c787c684c7
Mohammed N <mnaser11218@gmail.com>
2025-03-31 22:13:26 -0400

    updated player component to edit playername when user clicks edit button

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
+
 export default function Player({name, symbol}){
+    // const [player, setPlayer] = useState(name)
+    const [isEditing, setIsEditing] = useState(false)
+    const inputField = <input></input>
     return (
         <li>
         <span className="player">
-        <span className="player-name">{name}</span>
+        <span className="player-name">{ isEditing ? inputField : name }</span>
         <span className="player-symbol">{symbol}</span>
         </span>
-        <button>Edit</button>
+        <button onClick={()=> setIsEditing(!isEditing)}>Edit</button>
       </li> 
     )
 }
