87983748f73e709df860c042b3f48ddce2e94ad1
Mohammed N <mnaser11218@gmail.com>
2025-04-03 15:38:45 -0400

    added log to render logs in log component

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Log.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Log.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 function Log({turns}) {
   return (
     <ol id="log">
-        Log
+       {turns.map((turn)=>(
+        <li key={turn.square.row + turn.square.col}>
+            Square: row: {turn.square.row } col: {turn.square.col}, 
+            Player: {turn.player}
+        </li>
+       ))}
         </ol>
   )
 }
