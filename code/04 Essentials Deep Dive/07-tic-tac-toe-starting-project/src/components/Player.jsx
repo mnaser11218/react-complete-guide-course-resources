f61a099e610b0a47514c250a5f73a804f9d0ad42
Mohammed N <mnaser11218@gmail.com>
2025-04-05 13:10:12 -0400

    fixed bug in player component in which onNamechange unneccesarily re-renders

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,22 +1,30 @@
 import { useState, useEffect} from "react"
 
-export default function Player({initialName, symbol, isActive}){
+export default function Player({initialName, symbol, isActive, onNameChange}){
     const [player, setPlayer] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
     let playerName = player;
     let btnCaption = 'Edit'
     if(isEditing){
-        playerName = <input value={player} onChange={(e)=>setPlayer(e.target.value)} />
+        playerName = <input value={player} onChange={(e)=>setPlayer(e.target.value)} /> 
         btnCaption = 'Save'
     }
+
+    const handleEditClick = ()=>{
+        if(isEditing){
+            onNameChange(symbol, playerName)
+        }
+
+        setIsEditing((editing)=> !editing)
+    } 
+
     return (
-       
         <li className={isActive ? 'active' : undefined}> 
         <span className="player">
         <span className="player-name">{playerName}</span>
         <span className="player-symbol">{symbol}</span>
         </span>
-        <button onClick={()=> setIsEditing((editing)=> !editing)}>{btnCaption}</button>
+        <button onClick={handleEditClick}>{btnCaption}</button>
       </li> 
     
      
