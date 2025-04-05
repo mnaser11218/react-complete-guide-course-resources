d48e98ad875ac47392354c7ef46a0708155ee802
Mohammed N <mnaser11218@gmail.com>
2025-04-04 23:11:29 -0400

    pushed player name state up to app component

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect} from "react"
 
-export default function Player({initialName, symbol, isActive}){
-    const [player, setPlayer] = useState(initialName)
+export default function Player({updatePlayer,player, symbol, isActive}){
+    // const [player, setPlayer] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
     let playerName = player;
     let btnCaption = 'Edit'
     if(isEditing){
-        playerName = <input value={player} onChange={(e)=>setPlayer(e.target.value)} />
+        playerName = <input value={player} onChange={(e)=>updatePlayer(e.target.value)} />
         btnCaption = 'Save'
     }
     return (
