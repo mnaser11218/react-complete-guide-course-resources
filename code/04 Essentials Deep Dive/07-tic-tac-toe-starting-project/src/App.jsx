6a95dd395284d9bee3937ae0935bfb9f97417384
Mohammed N <mnaser11218@gmail.com>
2025-04-05 16:00:37 -0400

    optimized app component by creating helper methods and created helpermethods file to store state and helper methods

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -2,69 +2,41 @@ import Gameboard from "./components/Gameboard"
 import Log from "./components/Log"
 import Player from "./components/Player"
 import { useEffect, useState } from "react"
-import { WINNING_COMBINATIONS } from "./winning-combinations"
+import {deriveActivePlayer, deriveGameBoard,deriveWinner, PLAYERS, INITIAL_GAME_BOARD } from "./helperMethods"
 import GameOver from "./components/GameOver"
-let initialBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-]
-const deriveActivePlayer = (gameTurns)=> {
-  let currentPlayer = 'X'
-  if(gameTurns.length > 0 && gameTurns[0].player=== 'X'){
-    currentPlayer= 'O'
-  }
-  return currentPlayer;
-}
 
 function App() {
+  //STATE
   const [gameTurns, setGameTurns] = useState([])
   const activePlayer = deriveActivePlayer(gameTurns)
-  const [players, setPlayers]= useState({
-    X: 'Player One',
-    O: 'Player Two'
-  })
-  let gameBoard= [...initialBoard.map(board=>[...board])]
-  let winner = null;
+  const [players, setPlayers]= useState(PLAYERS)
+  const gameBoard = deriveGameBoard(gameTurns);
+  const winner = deriveWinner(gameBoard, players)
   let hasDraw = !winner && gameTurns.length === 9;
+
+
+
+  //METHODS
   const restartBoard = ()=>setGameTurns([])
-
-const handlePlayerNameChange = (symbol, playerName)=>{
-  setPlayers(prevPlayers=> ({...prevPlayers, [symbol]: playerName}))
-}
-  
-
-
-  for(const turn of gameTurns){
-      const {square, player}= turn;
-      const {row, col}=square;
-      gameBoard[row][col]=player;
-  }
-
-
-  for(const combination of WINNING_COMBINATIONS){
-    const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column]
-    const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column]
-    const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column]
-    if(firstSquareSymbol && firstSquareSymbol == secondSquareSymbol && firstSquareSymbol == thirdSquareSymbol){
-    winner= players[firstSquareSymbol];
-    }
-  }
+  const handlePlayerNameChange = (symbol, playerName)=>{setPlayers(prevPlayers=> ({...prevPlayers, [symbol]: playerName}))}
   const handleSelectSquare = (rowIndex, colIndex)=>{
-      setGameTurns((prevTurns)=>{
-      let currentPlayer = deriveActivePlayer(prevTurns)
-      const updatedTurns = [
-        {square: {row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns,
-      ]
-      return updatedTurns;
-    })
+  setGameTurns((prevTurns)=>{
+  let currentPlayer = deriveActivePlayer(prevTurns)
+  const updatedTurns = [
+    {square: {row: rowIndex, col: colIndex}, player: currentPlayer}, ...prevTurns,
+  ]
+  return updatedTurns;
+  })
   }
+
+
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player initialName="player one" symbol="X" isActive={activePlayer==='X'} onNameChange={handlePlayerNameChange} />
-          <Player initialName="player two" symbol="O" isActive={activePlayer==='O'} onNameChange={handlePlayerNameChange} />
+          <Player initialName={PLAYERS.X} symbol="X" isActive={activePlayer==='X'} onNameChange={handlePlayerNameChange} />
+          <Player initialName={PLAYERS.O} symbol="O" isActive={activePlayer==='O'} onNameChange={handlePlayerNameChange} />
         </ol>
         {(winner || hasDraw) ? <GameOver winner={winner} hasDraw={hasDraw} restartBoard={restartBoard} />: 
         "GAME BOARD"}
