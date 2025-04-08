74ddff7b7fdbc102a703ba5c0fb8d4d3ae38b010
Mohammed N <mnaser11218@gmail.com>
2025-04-08 18:39:53 -0400

    added second inputgroup state

--- code/05 Essentials Practice/01-starting-project/src/components/InputGroups.jsx
+++ code/05 Essentials Practice/01-starting-project/src/components/InputGroups.jsx
@@ -4,14 +4,13 @@ function InputGroups({ firstInput,secondInput,labelOne, onUpdateInputOne, second
   return (
     
     <div  className="input-group">
-
       <div>
       <label htmlFor="initial-investment">{labelOne}</label>
     <input type="text" id="initial-investment" onChange={(e)=>onUpdateInputOne(e.target.value)} value={firstInput}/>
    
     </div>
 
-    <div >
+    <div>
       <label htmlFor="annual-investment">{secondLabel}</label>
     <input id="annual-investment"  onChange={(e)=>onUpdateInputTwo(e.target.value)} value={secondInput} />
   
