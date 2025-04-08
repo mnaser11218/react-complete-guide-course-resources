c54a3033f68a5a4023700c93d4ddfad86ce30939
Mohammed N <mnaser11218@gmail.com>
2025-04-08 10:39:25 -0400

    updated input fields in app component

--- code/05 Essentials Practice/01-starting-project/src/App.jsx
+++ code/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -5,7 +5,56 @@ function App() {
     <>
     <header id="header">
     <img src={pic}/> 
+    <h1>Investment Calculator</h1>
     </header>
+    <div id="user-input"> 
+
+    <div  className="input-group">
+
+
+      <div>
+      <label for="initial-investment">Initial Investment </label>
+    <input id="initial-investment"></input>
+   
+    </div>
+
+
+
+
+    <div>
+      <label for="initial-investment">Initial Investment  </label>
+    <input id="initial-investment"></input>
+  
+    </div>
+    </div>
+
+   
+
+    <div className="input-group">
+
+
+
+      <div>
+    <label >Expected Return </label>
+    <input></input>
+   
+    </div>
+
+    <div>
+    <label >Duration </label>
+    <input></input>
+   
+    </div>
+
+
+    </div>
+    </div>
+
+ 
+
+
+
+
 
     
     </>
