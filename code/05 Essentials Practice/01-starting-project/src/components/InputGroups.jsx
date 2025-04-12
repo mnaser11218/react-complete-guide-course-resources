2d3f0fbd7273b8d233c61f85c83c69c1149f3f66
Mohammed N <mnaser11218@gmail.com>
2025-04-11 22:38:44 -0400

    added userInput component

--- code/05 Essentials Practice/01-starting-project/src/components/InputGroups.jsx
+++ code/05 Essentials Practice/01-starting-project/src/components/InputGroups.jsx
@@ -2,18 +2,14 @@ import React from 'react'
 
 function InputGroups({ firstInput,secondInput,labelOne, onUpdateInputOne, secondLabel,onUpdateInputTwo}) {
   return (
-    
     <div  className="input-group">
       <div>
       <label htmlFor="initial-investment">{labelOne}</label>
     <input type="text" id="initial-investment" onChange={(e)=>onUpdateInputOne(e.target.value)} value={firstInput}/>
-   
     </div>
-
     <div>
       <label htmlFor="annual-investment">{secondLabel}</label>
     <input id="annual-investment"  onChange={(e)=>onUpdateInputTwo(e.target.value)} value={secondInput} />
-  
     </div>
     </div>
   )
---
+++ code/05 Essentials Practice/01-starting-project/src/components/UserInput.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserInput({ firstInput,secondInput,labelOne, onUpdateInputOne, secondLabel,onUpdateInputTwo}) {
+  return (
+    <div  className="input-group">
+
+    </div>
+  )
+}
+
+export default UserInput
